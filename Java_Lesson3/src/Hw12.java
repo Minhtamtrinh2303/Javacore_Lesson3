fb7d69003479d902788d15ed8b1cdab605001cae
Minhtamtrinh2303 <114578738+Minhtamtrinh2303@users.noreply.github.com>
2022-10-31 10:42:16 +0700

    Hw12

--- Java_Lesson3/src/Hw12.java
+++ Java_Lesson3/src/Hw12.java
@@ -5,7 +5,6 @@ public class Hw12 {
      * Nhập vào 2 số tự nhiên m và n, sao cho m < n.
      * Hãy liệt kê các số chính phương trong đoạn [m,n]. Có bao nhiêu số chính phương?
      */
-
     public static void main(String[] args) {
         System.out.print("m = ");               //Natural Number
         int m = new Scanner(System.in).nextInt();
@@ -23,6 +22,16 @@ public class Hw12 {
             }
         }
         //Square Number
-
+        int count = 0; // calculate
+        System.out.print("Square Number(s) in [m,n] is(are): ");
+        for (int i = m; i <= n; i++) {
+            double squareNum = Math.sqrt(i) / Math.round(Math.sqrt(i));
+            if (squareNum == 1){
+                count++;
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println("");
+        System.out.println("There is(are) " +count+ " square number(s).");
     }
 }
